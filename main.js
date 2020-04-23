20fee2f43b7e0ff0ffb9c9479f1c2e9b39b1e3d1
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-04-23 12:00:11 -0300

    condiciones para los parametros

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 function elMensajePuedeRedactarse(texto, mensaje){
     let resultado=false;
+    if(texto==undefined || mensaje==undefined){
+        return resultado;
+    }else if(mensaje.length>texto.length){
+        return resultado;
+    }
     let textoEv=texto;
     let mensajeEv=mensaje;
 
