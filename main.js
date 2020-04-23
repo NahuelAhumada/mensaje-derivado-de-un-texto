53d84a999a7a51fa5d8c6f261939d42f6098e5a8
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-04-23 13:02:44 -0300

    comentarios del codigo agregados

--- main.js
+++ main.js
@@ -9,13 +9,16 @@ function elMensajePuedeRedactarse(texto, mensaje){
     let caracteres=texto.trim().toLowerCase();
     let mensajeEv=mensaje.trim().toLowerCase();
     //Recorro la lista de carcateres del texto fuente
-    for(let i =0;i<caracteres.length&&!resultado;i++){
+    for(let i =0;i<caracteres.length&&!resultado;i++){        
         let valor=caracteres[i];
+        //Pre: El caracter esta contenido en el mensaje
+        //Post: Borra el caracter del mensaje y continua hasta que el mensaje tengo 0 caracteres
         if(mensaje.includes(valor)){
             mensaje=mensaje.replace(valor,"");
             if(mensaje.length==0){
                 resultado=true;
             }
         }
+
     }
 }
