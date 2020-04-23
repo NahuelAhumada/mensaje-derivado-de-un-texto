ffcbb79a5dc5abd13a0669b7ed2fa4aa1605718f
Nahuel Ahumada <ahumada.nahuel35149@gmail.com>
2020-04-23 12:37:52 -0300

    Recorrido del arrat de caracteres de forma condicional

--- main.js
+++ main.js
@@ -5,8 +5,14 @@ function elMensajePuedeRedactarse(texto, mensaje){
     }else if(mensaje.length>texto.length){
         return resultado;
     }
-    
+    //Evaluo sin espacios y en minuscula
     let caracteres=texto.trim().toLowerCase();
     let mensajeEv=mensaje.trim().toLowerCase();
+    //Recorro la lista de carcateres del texto fuente
+    for(let i =0;i<caracteres.length&&!resultado;i++){
+        let valor=caracteres[i];
+        if(mensaje.includes(valor)){
 
+        }
+    }
 }
